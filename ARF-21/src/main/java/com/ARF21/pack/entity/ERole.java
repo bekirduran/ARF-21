104d5598b031ce626f474054000c1f3b494f77ec
sonx1354 <enezmutluoglu@gmail.com>
2021-08-18 13:16:12 +0300

    0

--- ARF-21/src/main/java/com/ARF21/pack/entity/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.ARF21.pack.entity;
-
-public enum ERole {
-	ROLE_USER,
-	ROLE_COMPANY,
-	ROLE_ADMIN
-}
--- ARF-21/src/main/java/com/ARF21/pack/security/services/UserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.ARF21.pack.security.services;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsService {
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
