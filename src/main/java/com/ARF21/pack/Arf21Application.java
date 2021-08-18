ee72f2e789f258f0e30c77ab3b8aade5aa1eb953
sonx1354 <enezmutluoglu@gmail.com>
2021-08-18 13:59:11 +0300

    v1.0.2

---
+++ src/main/java/com/ARF21/pack/Arf21Application.java
@@ -0,0 +1,13 @@
+package com.ARF21.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arf21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arf21Application.class, args);
+	}
+
+}
